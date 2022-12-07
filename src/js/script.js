38134036c6fac8a57846c2399e3d3ec98e3784c0
Bek <beks2992@gmail.com>
2022-12-07 21:22:32 +0600

    add

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const lineUp = document.querySelectorAll('.lineup'),
+    percent = document.querySelectorAll('.percent');
+
+percent.forEach((elem, i) => {
+    lineUp[i].style.width = elem.innerHTML;
+});
+
+
+
